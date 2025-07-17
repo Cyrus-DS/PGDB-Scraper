3aa7a308ade6b483db90c70d0a119c5d07c9484e
Cyrus <cyruswambugu91@gmail.com>
2025-07-17 15:25:19 +0300

    Adding comments on python script

--- plumber_data.py
+++ plumber_data.py
@@ -5,31 +5,36 @@ import csv
 with open('plumber_response.json', 'r') as f:
     data = json.load(f)
 
+# Extracting values associated with the top level key
 people = data.get('publicRegisterContacts', [])
 
-# Open CSV for writing
+# Create and open a new CSV file for writing
 with open('certified_tradespeople_all_years.csv', 'w', newline='', encoding='utf-8') as csvfile:
     writer = csv.DictWriter(csvfile, fieldnames=[
         'fullName', 'email', 'contactNumber', 'licenceType', 'licenceYear'
     ])
     writer.writeheader()
 
+    # Looping through each dictionery in the people list to get details
     for person in people:
         name = person.get('fullName', '')
         email = person.get('publicEmail', '')
         contact = person.get('publicPhone', '')
 
+        # Looping through list under licenceHistory key for current person
         for history in person.get('licenceHistory', []):
             all_licences = []
-            if history.get('current'):
+            if history.get('current'):# Add all current licenses to empty list
                 all_licences.append(history['current'])
-            if history.get('previous'):
+            if history.get('previous'):# Add all previous licenses to empty list
                 all_licences.append(history['previous'])
-
+            
+            # Looping through all_licences to extract needed fields
             for licence in all_licences:
                 licence_type = licence.get('licenceTypeName', '')
                 years = licence.get('licenceYears', [])
 
+                # Looping through years to check year and keywords
                 for year in years:
                     if int(year) >= 2015 and any(
                         keyword in licence_type
@@ -41,4 +46,4 @@ with open('certified_tradespeople_all_years.csv', 'w', newline='', encoding='utf
                             'contactNumber': contact,
                             'licenceType': licence_type,
                             'licenceYear': year
-                        })
+                        })# Writing a row to CSV for each valid license found 
